8da02474401b90dc9f2ee449ab1ff3c230908247
Mirjana Perić <mirjana.orlic4@gmail.com>
2020-04-14 17:49:26 +0200

    changes

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-$('.slider-first').slick({
+var $slider = $('.slider-first').slick({
     dots: false,
     infinite: true,
     arrows: false,
     speed: 500,
-    fade: true,
     cssEase: 'linear',
     autoplay: true,
-    swipeToSlide: true
+    swipeToSlide: true,
+    focusOnSelect: true
   });
 
+  var slick = $slider.slick('getSlick');
+  $slider.on('mousedown', '.slick-slide', function () {
+        var curSlide = slick.slideCount === slick.currentSlide ? 0 : slick.currentSlide + 1;
+        $slider.slick('slickGoTo', curSlide, true);
+        console.log(slick);
+    });
 
-  // 
-
-  $('.slider-second').slick({
+  var $sliderSecond = $('.slider-second').slick({
     dots: false,
     infinite: true,
     arrows: false,
     speed: 500,
-    fade: true,
     cssEase: 'linear',
     autoplay: true,
-    swipeToSlide: true
+    swipeToSlide: true,
+    focusOnSelect: true
   });
+
+  var slickSecond = $sliderSecond.slick('getSlick');
+  $sliderSecond.on('mousedown', '.slick-slide', function () {
+    var curSlide = slickSecond.slideCount === slickSecond.currentSlide ? 0 : slickSecond.currentSlide + 1;
+    $sliderSecond.slick('slickGoTo', curSlide, true);
+    console.log(slickSecond);
+});
