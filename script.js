9f6811cdbd9ef3028ddae39a1b028b06456fc558
Mirjana Perić <mirjana.orlic4@gmail.com>
2020-04-14 16:49:36 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+$('.slider-first').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    swipeToSlide: true
+  });
+
+
+  // 
+
+  $('.slider-second').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    swipeToSlide: true
+  });
