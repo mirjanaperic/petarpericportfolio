5bc5fc321308bf05d107875941b979cd4e78f1cd
Mirjana Perić <mirjana.orlic4@gmail.com>
2020-04-17 21:30:45 +0200

    changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var $slider = $('.slider-first').slick({
+var $slider = $('.slider-1').slick({
     dots: false,
     infinite: true,
     arrows: false,
@@ -16,7 +16,7 @@ var $slider = $('.slider-first').slick({
         console.log(slick);
     });
 
-  var $sliderSecond = $('.slider-second').slick({
+  var $slider2 = $('.slider-2').slick({
     dots: false,
     infinite: true,
     arrows: false,
@@ -27,9 +27,36 @@ var $slider = $('.slider-first').slick({
     focusOnSelect: true
   });
 
-  var slickSecond = $sliderSecond.slick('getSlick');
-  $sliderSecond.on('mousedown', '.slick-slide', function () {
-    var curSlide = slickSecond.slideCount === slickSecond.currentSlide ? 0 : slickSecond.currentSlide + 1;
-    $sliderSecond.slick('slickGoTo', curSlide, true);
-    console.log(slickSecond);
+  var slick2 = $slider2.slick('getSlick');
+  $slider2.on('mousedown', '.slick-slide', function () {
+    var curSlide = slick2.slideCount === slick2.currentSlide ? 0 : slick2.currentSlide + 1;
+    $slider2.slick('slickGoTo', curSlide, true);
+    console.log(slick2);
+});
+
+
+var $slider3 = $('.slider-3').slick({
+  dots: false,
+  infinite: true,
+  arrows: false,
+  speed: 500,
+  cssEase: 'linear',
+  autoplay: true,
+  swipeToSlide: true,
+  focusOnSelect: true
+});
+
+var slick3 = $slider3.slick('getSlick');
+$slider3.on('mousedown', '.slick-slide', function () {
+  var curSlide = slick3.slideCount === slick3.currentSlide ? 0 : slick3.currentSlide + 1;
+  $slider3.slick('slickGoTo', curSlide, true);
+  console.log(slick3);
+});
+
+
+var logo = document.getElementById('logo');
+var logoWrap = document.getElementById('logo-wrap');
+
+logo.addEventListener('mouseover', function() {
+    logoWrap.innerHTML = 'petarpericportfolio';
 });
